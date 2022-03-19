54ca54859399ce89be83e5afb93d9b61e124e3a8
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-03-19 20:03:13 +0700

    Initial commit

---
+++ src/test/java/ru/netology/domain/ApiTest.java
@@ -0,0 +1,24 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class ApiTest {
+    @Test
+    void shouldReturnDemoAccounts() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+
+        ;
+    }
+}
